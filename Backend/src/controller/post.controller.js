79ce53033d98b8f88f8d67a1cafa5abe71a19e31
Rutu <rutubhimani124@gmail.com>
2024-05-14 11:22:16 +0530

    changes:getPost Api Updated

--- Backend/src/controller/post.controller.js
+++ Backend/src/controller/post.controller.js
@@ -25,7 +25,7 @@ const addPost = asyncHandler(async (req, res) => {
     console.log(lname);
     let name = fname + ' ' + lname
     console.log(name);
-    const { title,description,imgurl} = req.body
+    const { title, description, imgurl } = req.body
     // const postLocalPath = req.files?.postphoto[0]?.path;
     // if (!postLocalPath) {
     //     throw new ApiError(400, "Please upload post localpath is not found")
@@ -36,25 +36,76 @@ const addPost = asyncHandler(async (req, res) => {
     // if (!uploadPost) {
     //     throw new ApiError(400, "Post not upload on server")
     // }
-    console.log(title,description,imgurl)
+    // console.log(title, description, imgurl)
     const post = await Post.create({
         title,
         description,
-        postphoto:imgurl,
+        postphoto: imgurl,
         owner: name,
         user: user._id,
     });
-    if(!post) {
+    if (!post) {
         throw new ApiError(400, "Post not created")
     }
-    res.status(200).json(new ApiResponse(200,post, "Post uploaded successfully"))
+   const data = await Post.aggregate([
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user"
+            }
+        }
+    ]) 
+    console.log(data)
+
+    res.status(200).json(new ApiResponse(200, post, "Post uploaded successfully"))
 })
 
 const getPosts = asyncHandler(async (req, res) => {
     try {
         var page = Number(req.query.page)
         var post_data;
-        post_data = await Post.find().sort({ "updatedAt": 1 }).limit(7)
+        post_data = await Post.aggregate([
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "user"
+                }
+            },
+            {
+                $project: {
+                    "user.password": 0,
+                    "user.refreshToken": 0,
+                    "user.createdAt": 0,
+                    "user.updatedAt": 0,
+                    "user.isVerified": 0,
+                    "user.forgotPassword": 0,
+                    
+                }
+            },
+            {
+                $sort: {
+                    createdAt: -1
+                }
+            },
+            {
+                $skip: 10 * (page - 1)
+            },
+            {
+                $limit: 10
+            },
+            {
+                $addFields: {
+                    "user": {
+                        $arrayElemAt: ["$user", 0]
+                    }
+                }
+            }
+
+        ]) 
         res.status(200).json(new ApiResponse(200, { post_data }, "Posts fetched successfully"))
     } catch (error) {
         res.status(404).json(new ApiError(400, { "error": error }, "Something Went Worong"));
